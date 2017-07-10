aa793ade9ff338096b8056ce8439a6d31e45123f
BlackBeardEC <rich.stadnick@gmail.com>
2017-07-10 16:38:43 -0400

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,56 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User',{
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      isAsync: false,
+      validator: validator.isEmail,
+      message: '{value} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function(){
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function(){//can't be an arrow function ()=>{} becuase arrow functions can't bind the 'this' keyword.
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'pitbulls').toString();
+
+  user.tokens.push({access, token});
+  return user.save().then(()=>{
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
